ac5bcee84257091b1598c15b1711d0c83d726dba
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-15 21:38:40 +0800

    Code the four arithmetic functions and operate function

---
+++ js-file.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+    }
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
