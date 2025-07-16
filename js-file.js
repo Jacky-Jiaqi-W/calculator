8ed7de704b7d906da9b4dd85ab6efda02b1482a1
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-16 21:53:38 +0800

    Make the calculator work

--- js-file.js
+++ js-file.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return Math.round(a / b * 100) / 100;
 }
 
 function operate(operator, firstNum, secondNum) {
@@ -27,6 +27,102 @@ function operate(operator, firstNum, secondNum) {
     }
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+function evaluate() {
+    let arr = displayContent.split(" ");
+    firstNumber = +arr[0];
+    operator = arr[1];
+    secondNumber = +arr[2];
+    return operate(operator, firstNumber, secondNumber);
+}
+
+function divideByZero(arr) {
+    return (arr[1] === "/") && (arr[2] === "0");
+}
+
+function handlePressedNumbersBtn(pressedOperatorBtn) {
+    if (resultDisplay.textContent !== "") {
+        handlePressedClearBtn();
+    }
+    displayContent += pressedOperatorBtn.textContent;
+    populateDisplay(equationDisplay, displayContent);
+}
+
+function handlePressedOperatorsBtn(pressedOperatorBtn) {
+    if (displayContent !== "") {
+        let arr = displayContent.trimEnd().split(" ");
+        if (arr.length === 1) {
+            displayContent = `${displayContent} ${pressedOperatorBtn.textContent} `;
+        } else if (arr.length === 2) {
+            arr[1] = pressedOperatorBtn.textContent;
+            displayContent = arr.join(" ") + " ";;
+        } else if (arr.length === 3) {
+            if (divideByZero(arr)) {
+                handlePressedClearBtn();
+                populateDisplay(resultDisplay, "Error");
+                return;
+            }
+            arr.length = 0;
+            arr[0] = evaluate();
+            arr[1] = pressedOperatorBtn.textContent;
+            displayContent = arr.join(" ") + " ";
+            resultDisplay.textContent = "";
+        }
+        populateDisplay(equationDisplay, displayContent);
+    }
+}
+
+function handlePressedEvalBtn() {
+    let arr = displayContent.trimEnd().split(" ");
+    if (arr.length === 3) {
+        if (divideByZero(arr)) {
+            handlePressedClearBtn();
+            populateDisplay(resultDisplay, "Error");
+            return;
+        }
+        populateDisplay(resultDisplay, evaluate());
+    }
+}
+
+function handlePressedClearBtn() {
+    firstNumber = null;
+    secondNumber = null;
+    operator = "";
+    displayContent = "";
+    populateDisplay(equationDisplay, "");
+    populateDisplay(resultDisplay, "");
+}
+
+function handlePressedBtn(pressedBtn) {
+    switch (pressedBtn.className) {
+        case "number":
+            handlePressedNumbersBtn(pressedBtn);
+            break;
+        case "operators":
+            handlePressedOperatorsBtn(pressedBtn);
+            break;
+        case "operators eval":
+            handlePressedEvalBtn();
+            break;
+        case "ac":
+            handlePressedClearBtn();
+            break;
+    }
+}
+
+const equationDisplay = document.querySelector(".equation");
+const resultDisplay = document.querySelector(".result");
+
+function populateDisplay(targetElement, displayContent) {
+    targetElement.textContent = displayContent;
+}
+
+const keypad = document.querySelector(".keypad");
+
+keypad.addEventListener("click", e => {
+    handlePressedBtn(e.target);
+});
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = "";
+let displayContent = "";
